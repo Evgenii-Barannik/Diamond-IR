74769ff7d093c5aea6318b9b0e552d0274193458
Evgenii-Barannik <radchemsu@gmail.com>
2021-03-28 20:45:07 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from __future__ import print_function
+from ipywidgets import interact, interactive, fixed, interact_manual
+import ipywidgets as widgets
+
+import math #Math for math.log, math.exp
+import pandas as pd #Pandas for dataframes
+import seaborn as sns #Seaborn for plots
+import matplotlib.pyplot as plt #To configure plot
